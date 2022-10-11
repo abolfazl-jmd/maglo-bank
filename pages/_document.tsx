f9a71a17eea783ca41f50cb0b047d463a16007ce
Abolfazl <abolfazljamshididev@gmail.com>
2022-10-11 09:16:01 +0330

    add font family configuration to tailwindcss

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="light">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Kumbh+Sans:wght@200;300;400;500;600;700;800&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ const Home: NextPage = () => {
   return (
     <div className="bg-primary h-screen">
       <div className="border w-40 xl:w-full">hi div</div>
-      <h1 className="text-2xl text-slate-900 dark:text-textBlack">
+      <h1 className="text-2xl text-slate-900 dark:text-textBlack font-gortida">
         Hello Maglo.
       </h1>
     </div>
